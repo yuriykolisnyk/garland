1837d1949563391220dcc00bccc47432d8069418
yuriykolisnyk <81742552+yuriykolisnyk@users.noreply.github.com>
2021-11-11 12:04:55 +0000

    Deploying to gh-pages from @ yuriykolisnyk/garland@c609f3e4907903569a0883bcf12258b93b0f4ea5 🚀

--- src.637366ba.js
+++ src.637366ba.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/garland/src.3441d526.js.map
+},{"./../images/new-year.jpg":[["new-year.a38366e2.jpg","mG82"],"mG82"],"./../images/garland.png":[["garland.e18dfb9c.png","wPpd"],"wPpd"]}],"PR6Z":[function(require,module,exports) {
+function n(){nums=document.getElementById("nums_1").innerHTML,1==nums&&(document.getElementById("garland").className="garland_1",document.getElementById("nums_1").innerHTML="2"),2==nums&&(document.getElementById("garland").className="garland_2",document.getElementById("nums_1").innerHTML="3"),3==nums&&(document.getElementById("garland").className="garland_3",document.getElementById("nums_1").innerHTML="4"),4==nums&&(document.getElementById("garland").className="garland_4",document.getElementById("nums_1").innerHTML="1")}setInterval(function(){n()},300);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/garland.js");
+},{"./sass/main.scss":"clu1","./js/garland.js":"PR6Z"}]},{},["Focm"], null)
+//# sourceMappingURL=/garland/src.637366ba.js.map
